ba9c125a775c664aeb8e1d91fec49068633b6e77
Your_Name <joseph161216@gmail.com>
2020-06-26 22:09:55 -0700

    add test to go to correct page when clicking on logo

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -15,5 +16,24 @@ describe("Header", () => {
     // we are getting a link but there are multiple links, so we have to specify a name option with a regex value
     const link = screen.getByRole("link", { name: /how it works/i })
     screen.debug(link);
+    userEvent.click(link);
+    expect(
+      screen.getByRole("heading", { name: /how it works/i })
+    ).toBeInTheDocument();
   });
+
+  test('"Logo": link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const link = screen.getByRole("link", { name: /logo.svg/i })
+    screen.debug(link);
+    userEvent.click(link);
+    expect(
+      screen.getByRole("button", { name: /search/i })
+    ).toBeInTheDocument();
+  })
+  
 });
