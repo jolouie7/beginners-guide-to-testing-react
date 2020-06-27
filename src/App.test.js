70656743c75bef54ef7dca957296bfcc33bde530
Your_Name <joseph161216@gmail.com>
2020-06-26 23:03:21 -0700

    add test to check about link is going to right page

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,11 @@ import userEvent from "@testing-library/user-event";
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
+// TODO: use test.each https://jestjs.io/docs/en/api#testeachtablename-fn-timeout to DRY up code for how it works and about links.
+
 describe("Header", () => {
-  test('"How it works: link points to the correct page', () => {
+  test('how it works: link points to the correct page', () => {
+    console.log(name)
     //During tests we wrap the App inside a MemoryRouter
     render(
       <MemoryRouter>
@@ -15,10 +18,25 @@ describe("Header", () => {
     // getByRole should be your go-to query.
     // we are getting a link but there are multiple links, so we have to specify a name option with a regex value
     const link = screen.getByRole("link", { name: /how it works/i })
-    screen.debug(link);
+    // screen.debug(link);
     userEvent.click(link);
     expect(
       screen.getByRole("heading", { name: /how it works/i })
+    ).toBeInTheDocument();
+  });
+
+  test('about: link points to the correct page', () => {
+    console.log(name)
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const link = screen.getByRole("link", { name: /about/i })
+    // screen.debug(link);
+    userEvent.click(link);
+    expect(
+      screen.getByRole("heading", { name: /about/i })
     ).toBeInTheDocument();
   });
 
@@ -29,7 +47,7 @@ describe("Header", () => {
       </MemoryRouter>
     );
     const link = screen.getByRole("link", { name: /logo.svg/i })
-    screen.debug(link);
+    // screen.debug(link);
     userEvent.click(link);
     expect(
       screen.getByRole("button", { name: /search/i })
