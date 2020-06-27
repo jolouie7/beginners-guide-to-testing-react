0342e9da244754fb806d4164ad8b4a3c8392b4db
Your_Name <joseph161216@gmail.com>
2020-06-26 21:38:47 -0700

    add test to check How it works: link points to the correct page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Header", () => {
+  test('"How it works: link points to the correct page', () => {
+    //During tests we wrap the App inside a MemoryRouter
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    // getByRole should be your go-to query.
+    // we are getting a link but there are multiple links, so we have to specify a name option with a regex value
+    const link = screen.getByRole("link", { name: /how it works/i })
+    screen.debug(link);
+  });
 });
